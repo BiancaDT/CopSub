47376788fa3787accc78cb6fffa3dff77083ffad
BiancaDT <bianca.dianat@gmail.com>
2017-04-10 21:41:54 +0200

    Initial commit

---
+++ app/src/main/java/bianca/copsub/MainActivity.java
@@ -0,0 +1,18 @@
+package bianca.copsub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //for placing icon
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.csi);
+    }
+
+    }
+
